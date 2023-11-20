17d596dce5174c55c79d2a402c28a585a4b449ce
makhalifa26 <adil_khalifa@me.com>
2023-11-20 17:11:11 +0000

    Lesson 1 Javascript

---
+++ Javascript/Lesson1.js
@@ -0,0 +1,16 @@
+console.log(1+1)
+
+const myname= "Mohammad"
+console.log(myname)
+
+var current="November"
+console.log(current)
+
+var previous="October"
+console.log(previous)
+
+let next="December"
+console.log(next)
+
+next="January"
+console.log(next)
